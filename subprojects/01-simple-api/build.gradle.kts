27b0c86014c81ec4ad30aafceb82aceb00787867
Benjamin Allion <benjamin.allion.pro@gmail.com>
2025-05-14 22:42:17 +0200

    fix(simple-api): fix simple-api issues. (gradle + useless character)

--- subprojects/01-simple-api/build.gradle.kts
+++ subprojects/01-simple-api/build.gradle.kts
@@ -1,16 +1,25 @@
 plugins {
+	id("java")
 	id("org.springframework.boot")
 	id("io.spring.dependency-management")
 }
 
-version = "0.0.1-SNAPSHOT"
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+
+version = "1.0-SNAPSHOT"
 
 dependencies {
-	implementation("org.springframework:spring-context:6.2.2")
-	implementation("org.springframework.boot:spring-boot-starter:3.4.2")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.4.2")
+	implementation("org.springframework:spring-context")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.jar {
