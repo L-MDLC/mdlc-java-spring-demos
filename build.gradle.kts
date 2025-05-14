3d32eb7cb47f313382c463bcc42e3a76a62da961
Benjamin Allion <benjamin.allion.pro@gmail.com>
2025-05-14 22:43:53 +0200

    chore(project): add spring 'milestone' repository & downgrade java to 17
    To avoid a lot of compatibility issues.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     kotlin("jvm") version "2.1.20" apply false
-    id("org.springframework.boot") version "3.4.2" apply false
+    id("org.springframework.boot") version "3.3.0" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
@@ -9,21 +9,24 @@ allprojects {
     group = "edu.lmdlc.demo"
     repositories {
         mavenCentral()
+        maven {
+            url = uri("https://repo.spring.io/milestone")
+            name = "Spring Milestones"
+        }
     }
 }
 
 subprojects {
     apply(plugin = "java") // Apply Java plugin for Java sub-project configurations
-    apply(plugin = "kotlin") // Apply Kotlin plugin (if required) for Kotlin sub-project configurations
 
     dependencies {
-        compileOnly("org.projectlombok:lombok:1.18.36")
-        annotationProcessor("org.projectlombok:lombok:1.18.36")
+        compileOnly("org.projectlombok:lombok:1.18.30")
+        annotationProcessor("org.projectlombok:lombok:1.18.30")
     }
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(23))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
 }
